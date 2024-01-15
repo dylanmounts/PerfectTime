b862e219ce3a91fb7e8bcbdfaf7108343e81c7a4
Dylan Mounts <dmounts@gmail.com>
2024-01-15 10:40:46 -0600

    Include bootstrap via vite.

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -1,10 +1,33 @@
+import { Toast } from 'bootstrap';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
+import '../frontend/scss/styles.scss'
 import { addClock } from './modules/clock/clockConstructor';
 import { updateClock } from './modules/clock/clockUpdater';
 import { timeManager } from './modules/managers/timeManager';
 
+
+// Bootstrap
+const optionsTrigger = document.getElementById('optionsMenuBtn')
+const optionsMenu = document.getElementById('optionsMenu')
+const infoTrigger = document.getElementById('infoMenuBtn')
+const infoMenu = document.getElementById('infoMenu')
+
+if (optionsTrigger) {
+        const toastBootstrap = Toast.getOrCreateInstance(optionsMenu)
+        optionsTrigger.addEventListener('click', () => {
+            toastBootstrap.show()
+    })
+}
+if (infoTrigger) {
+        const toastBootstrap = Toast.getOrCreateInstance(infoMenu)
+        infoTrigger.addEventListener('click', () => {
+            toastBootstrap.show()
+    })
+}
+
+// Three
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,31 @@
-import { defineConfig } from 'vite';
 import copy from 'rollup-plugin-copy';
 
-export default defineConfig({
-    plugins: [
-      copy({
-        targets: [
-          { src: 'src/assets/fonts/*', dest: 'dist/fonts' }
-        ],
-        hook: 'writeBundle'
-      })
-    ],
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              const directories = id.split('/');
-              const name = directories[directories.lastIndexOf('node_modules') + 1];
-              return name;
-            }
+const path = require('path')
+
+export default {
+  plugins: [
+    copy({
+      targets: [
+        { src: 'src/assets/fonts/*', dest: 'dist/fonts' }
+      ],
+      hook: 'writeBundle'
+    })
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            const directories = id.split('/');
+            const name = directories[directories.lastIndexOf('node_modules') + 1];
+            return name;
           }
         }
-      },
-      chunkSizeWarningLimit: 600
-    }
-  });
+      }
+    },
+    chunkSizeWarningLimit: 600
+  },
+  server: {
+    port: 8080
+  }
+}
