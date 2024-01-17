7c2fcb9c1d34c2eff94d4fa4a0e5c59f24d7efd1
Dylan Mounts <dmounts@gmail.com>
2024-01-17 14:34:43 -0600

    Commented fontManager.js.

--- src/frontend/modules/managers/fontManager.js
+++ src/frontend/modules/managers/fontManager.js
@@ -1,3 +1,12 @@
+/**
+ * fontManager.js - Manages font resources for the perfect clock.
+ *
+ * This module provides a FontManager class for loading and handling fonts
+ * using Three.js. We use two different font managers to handle the two
+ * different (regular and monospaced) clock fonts.
+ */
+
+
 import * as THREE from 'three';
 import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
@@ -10,6 +19,12 @@ class FontManager {
         this.fontPromise = this.loadFont(url);
     }
 
+    /**
+     * Asynchronously loads a font from the provided URL.
+     * 
+     * @param {string} url - The URL of the font to load.
+     * @returns {Promise} A promise that resolves to the loaded font.
+     */
     loadFont(url) {
         return new Promise((resolve, reject) => {
             const loader = new FontLoader();
@@ -23,6 +38,13 @@ class FontManager {
         });
     }
 
+    /**
+     * Creates a text mesh using the loaded font.
+     * 
+     * @param {string} text - The text to display with the mesh.
+     * @param {Object} options - Options for creating the text mesh.
+     * @returns {THREE.Mesh} A Three.js mesh displaying the text.
+     */
     createTextMesh(text, options) {
         if (!this.font) {
             console.error('Font not loaded');
@@ -42,6 +64,11 @@ class FontManager {
         return new THREE.Mesh(geometry, options.material);
     }
 
+    /**
+     * Ensures the font is loaded and returns it.
+     * 
+     * @returns {THREE.Font} The loaded Three.js font.
+     */
     async getLoadedFont() {
         await this.fontPromise;
         return this.font;
