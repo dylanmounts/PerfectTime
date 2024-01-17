7a78b541958e1b77778e214381060ca5ebbf0832
Dylan Mounts <dmounts@gmail.com>
2024-01-17 14:37:27 -0600

    Commented shapes.js.

--- src/frontend/modules/visuals/shapes.js
+++ src/frontend/modules/visuals/shapes.js
@@ -1,9 +1,18 @@
 
+/**
+ * shapes.js - Defines shapes for the perfect clock.
+ */
+
 import * as THREE from 'three';
 
 import { SIZES } from '../constants.js';
 
 
+/**
+ * Creates the shape for the hour hand of the clock.
+ * 
+ * @returns {THREE.Shape} The shape of the hour hand.
+ */
 export const hourHandShape = () => {
     const shape = new THREE.Shape();
     shape.moveTo(0, -0.3);
@@ -14,6 +23,11 @@ export const hourHandShape = () => {
     return shape;
 };
 
+/**
+ * Creates the shape for the minute hand of the clock.
+ * 
+ * @returns {THREE.Shape} The shape of the minute hand.
+ */
 export const minuteHandShape = () => {
     const shape = new THREE.Shape();
     shape.moveTo(0, -0.2);
@@ -24,6 +38,11 @@ export const minuteHandShape = () => {
     return shape;
 };
 
+/**
+ * Creates the shape for the second hand of the clock.
+ * 
+ * @returns {THREE.Shape} The shape of the second hand.
+ */
 export const secondHandShape = () => {
     const shape = new THREE.Shape();
     shape.moveTo(0, -0.1);
