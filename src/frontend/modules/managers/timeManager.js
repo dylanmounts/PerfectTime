7f060a1137da0b4d2e1b09caa8b4d75f5c4dfed2
Dylan Mounts <dmounts@gmail.com>
2024-01-17 14:14:45 -0600

    Commented timeManager.js.

--- src/frontend/modules/managers/timeManager.js
+++ src/frontend/modules/managers/timeManager.js
@@ -1,3 +1,12 @@
+/**
+ * timeManager.js - Manages time synchronization for PerfectTime.org.
+ *
+ * This module defines a TimeManager class responsible for synchronizing the clock
+ * with backend timeserver to ensure accuracy. It fetches the perfect time from the
+ * time server's API and calculates the offset from the user's local time.
+ */
+
+
 class TimeManager {
     constructor() {
         this.perfectTimeEndpoint = 'https://perfecttime.org/api/time'
@@ -6,6 +15,12 @@ class TimeManager {
         this.timeOffset = null;
     }
 
+    /**
+     * Fetches the perfect time from the time server and calculates the time offset.
+     *
+     * This function tries to fetch the current time from the backend time server. In case
+     * of an error, it falls back to the local system time and sets the time offset to zero.
+     */
     async fetchPerfectTime() {
         try {
             const response = await fetch(this.perfectTimeEndpoint);
@@ -20,6 +35,14 @@ class TimeManager {
         }
     }
 
+    /**
+     * Calculates and returns the current corrected time.
+     *
+     * This method returns the current time perfected by the time offset. If something went
+     * wrong and the offset isn't available it returns the local system time.
+     *
+     * @returns {Date} The current corrected time.
+     */
     getCurrentTime() {
         if (!this.perfectTime) {
             return new Date();
