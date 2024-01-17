ddd6f61efbf0aaede4d46f0625a82385fb770552
Dylan Mounts <dmounts@gmail.com>
2024-01-17 15:38:53 -0600

    Commented app.js.

--- app.js
+++ app.js
@@ -9,20 +9,27 @@ const hostname = process.env.HOSTNAME || '127.0.0.1';
 
 app.use(cors());
 
+// Serve static files from the 'dist' directory
 app.use(express.static(path.join(__dirname, 'dist')));
 
+// Endpoint to get the perfect time
 app.get('/api/time', (req, res) => {
     const currentTime = timeServer.getPerfectTime();
     res.json({ time: currentTime });
 });
 
+// Serve the main index.html for any other requests
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
+// Start the server
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
 
+// Initial backend time synchronization with NTP server
 timeServer.updateTimeFromNTP();
+
+// Update time from NTP server every 15 minutes
 setInterval(timeServer.updateTimeFromNTP, 15 * 60 * 1000);
