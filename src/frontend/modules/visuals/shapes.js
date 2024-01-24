0173d1bc55a66bf0f6e58dc554b11a64aac0fe27
Dylan Mounts <dmounts@gmail.com>
2024-01-24 11:40:16 -0600

    Implemented additional constants for creating clock hand shapes.

--- src/frontend/modules/visuals/shapes.js
+++ src/frontend/modules/visuals/shapes.js
@@ -14,17 +14,18 @@ import { SIZES } from '../constants.js';
  * @param {number} tipWidth - Width of the tip of the clock hand.
  * @param {number} baseWidth - Width of the base of the clock hand.
  * @param {number} baseOffset - Offset of the base of the clock hand from the clock's center.
- * @param {number} handLength - Proportional length of the clock hand relative to the clock's radius.
+ * @param {number} handLengthRatio - Proportional length of the clock hand relative to the clock's radius.
  * @param {number} [scale=1] - Optional scaling factor to adjust the overall size of the clock hand.
  * @returns {THREE.Shape} A THREE.Shape object representing the clock hand.
  */
-export const createClockHand = (tipWidth, baseWidth, baseOffset, handLength, scale = 1) => {
-    const lengthScale = scale === 1 ? scale : 0.9925;
+export const createClockHand = (tipWidth, baseWidth, baseOffset, handLengthRatio, scale = 1) => {
+    const lengthScale = scale === 1 ? scale : SIZES.OUTER_HAND_LENGTH_SCALE;
+    const tipLength = SIZES.CLOCK_RADIUS * handLengthRatio * lengthScale;
+    const triangleHeight = tipWidth * 3;
+
     baseWidth = baseWidth * scale;
     baseOffset = baseOffset * scale;
-    const tipLength = SIZES.CLOCK_RADIUS * handLength * lengthScale;
     tipWidth = tipWidth * scale;
-    const triangleHeight = tipWidth * 2;
 
     const shape = new THREE.Shape();
     shape.moveTo(0, -baseOffset);
