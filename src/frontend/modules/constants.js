588da765d57c713c80930c2b1ea03680bf07e880
Dylan Mounts <dmounts@gmail.com>
2024-02-09 09:10:15 -0600

    Removed several unused sizes.

--- src/frontend/modules/constants.js
+++ src/frontend/modules/constants.js
@@ -27,19 +27,16 @@ export const SIZES = {
     BEZEL_RADIUS: 1.0,
     CLOCK_RADIUS: 5.0,
     CLOCK_THICKNESS: 1.0,
-    DAY_DATE_BOX_DEPTH: 0.05,
-    DAY_DATE_FRAME_DEPTH: 0.0575,
-    DAY_DATE_FRAME_THICKNESS: 0.02,
+    DAY_DATE_BOX_DEPTH: 0.06,
     DAY_DATE_NUMBER_HEIGHT: 0.0125,
     DAY_DATE_SIZE: 0.28125,
-    DAY_DATE_SPACING: 0.145,
     DAY_DATE_BOX_HEIGHT: 0.45,
     DAY_DATE_BOX_WIDTH: 2.2,
     DAY_DATE_BEVEL_SIZE: .025,
     DAY_DATE_BEVEL_THICKNESS: 0.001,
     DIGITAL_DISPLAY_BEVEL_SIZE: .0275,
     DIGITAL_DISPLAY_BEVEL_THICKNESS: 0.001,
-    DIGITAL_DISPLAY_BOX_DEPTH: 0.05,
+    DIGITAL_DISPLAY_BOX_DEPTH: 0.06,
     DIGITAL_DISPLAY_BOX_HEIGHT: 0.725,
     DIGITAL_DISPLAY_BOX_WIDTH: 2.8925,
     DIGITAL_DISPLAY_NUMBER_HEIGHT: 0.0125,
@@ -47,8 +44,6 @@ export const SIZES = {
     INDICATOR_BEVEL_THICKNESS: 0.001,
     INDICATOR_BEVEL_SIZE: 0.02,
     INDICATOR_HEIGHT: 0.025,
-    INDICATOR_RADIUS: 0.15,
-    INDICATOR_SCALE: 0.8,
     NUMBER_HEIGHT: 0.01,
     NUMBER_SIZE: 0.5,
     NUMBER_BEVEL_SIZE: 0.0275,
@@ -59,24 +54,20 @@ export const SIZES = {
     HOUR_HAND_TIP_WIDTH: 0.048,
     HOUR_HAND_BASE_WIDTH: 0.51,
     HOUR_HAND_BASE_OFFSET: 0.3,
-    HOUR_HAND_LENGTH_RATIO: 5/8,
     MINUTE_HAND_SCALE: 0.80,
     MINUTE_HAND_TIP_WIDTH: 0.036,
     MINUTE_HAND_BASE_WIDTH: 0.3825,
     MINUTE_HAND_BASE_OFFSET: 0.3,
-    MINUTE_HAND_LENGTH_RATIO: 23/24,
     SECOND_HAND_SCALE: 0.5,
     SECOND_HAND_TIP_WIDTH: 0.015,
     SECOND_HAND_BASE_WIDTH: 0.080,
     SECOND_HAND_BASE_OFFSET: 0.1,
-    SECOND_HAND_LENGTH_RATIO: 23/24,
-    OUTER_HAND_LENGTH_SCALE: 0.9925
 };
 
-export const CLOCK_HOURS_FONT = 'fonts/noto_serif_regular.typeface.json';
-export const CLOCK_MINUTES_FONT = 'fonts/noto_sans_regular.typeface.json';
-export const CLOCK_DAY_DATE_FONT = 'fonts/noto_serif_light_regular.typeface.json';
-export const CLOCK_DIGITAL_FONT = 'fonts/noto_sans_light_regular.typeface.json';
+export const CLOCK_HOURS_FONT = 'dist/fonts/noto_serif_regular.typeface.json';
+export const CLOCK_MINUTES_FONT = 'dist/fonts/noto_sans_regular.typeface.json';
+export const CLOCK_DAY_DATE_FONT = 'dist/fonts/noto_serif_light_regular.typeface.json';
+export const CLOCK_DIGITAL_FONT = 'dist/fonts/noto_sans_light_regular.typeface.json';
 export const CLOCK_OUTER_RADIUS = SIZES.CLOCK_RADIUS + SIZES.BEZEL_THICKNESS;
 export const DAY_DATE_BASE_Y = -SIZES.CLOCK_RADIUS * 1/6;
 export const DIGITAL_DISPLAY_BASE_Y = SIZES.CLOCK_RADIUS * 1/6;
